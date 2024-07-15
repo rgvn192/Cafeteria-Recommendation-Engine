be1b98d69bb1c64202d27b8a1e90cd7d1eb7152f
Raghvendra Chauhan <raghvendra.s@intimetec.com>
2024-07-15 22:23:20 +0530

    Added Unit Test for Sentiment Analyzer

---
+++ RecommendationEngine/UnitTest/Services/SentimentAnalyzerTests.cs
@@ -0,0 +1,74 @@
+﻿using Server.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Services
+{
+
+    [TestClass]
+    public class SentimentAnalyzerTests
+    {
+        private SentimentAnalyzer _sentimentAnalyzer;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _sentimentAnalyzer = new SentimentAnalyzer();
+        }
+
+        [TestMethod]
+        public void AnalyzeSentiment_ShouldReturnHighPositiveScore_WhenCommentIsPositive()
+        {
+            string comment = "This is a delicious and amazing dish!";
+
+            decimal score = _sentimentAnalyzer.AnalyzeSentiment(comment);
+
+            Assert.IsTrue(score > 3.5m);
+        }
+
+        [TestMethod]
+        public void AnalyzeSentiment_ShouldReturnLowNegativeScore_WhenCommentIsNegative()
+        {
+            string comment = "This is a terrible and disgusting dish!";
+
+            decimal score = _sentimentAnalyzer.AnalyzeSentiment(comment);
+
+            Assert.IsTrue(score < 1.5m);
+        }
+
+        [TestMethod]
+        public void AnalyzeSentiment_ShouldReturnNeutralScore_WhenCommentIsNeutral()
+        {            
+            string comment = "The dish was okay.";
+
+            decimal score = _sentimentAnalyzer.AnalyzeSentiment(comment);
+
+            Assert.IsTrue(score >= 2.0m && score <= 3.0m);
+        }
+
+        [TestMethod]
+        public void AnalyzeSentiment_ShouldHandleNegationCorrectly()
+        {
+            string comment = "This dish is not delicious and not terrible.";
+
+            decimal score = _sentimentAnalyzer.AnalyzeSentiment(comment);
+
+            Assert.IsTrue(score >= 2.0m && score <= 3.0m);
+        }
+
+        [TestMethod]
+        public void AnalyzeSentiment_ShouldReturnCorrectScore_WhenCommentIsMixed()
+        {
+            string comment = "The food was tasty but the service was awful.";
+
+            decimal score = _sentimentAnalyzer.AnalyzeSentiment(comment);
+
+            Assert.IsTrue(score >= 2.0m && score <= 3.0m);
+        }
+    }
+}
+
+
